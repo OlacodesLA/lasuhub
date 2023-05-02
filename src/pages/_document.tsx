0da5580a9c5a58e4d8788678e66367a812574555
OlacodesLA <zaccheausjide@gmail.com>
2023-05-02 13:24:29 +0800

    added a key to a map

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,4 @@
+//@ts-nocheck
 import { Html, Head, Main, NextScript } from 'next/document'
 import Transition from '@/components/Transition'
 
@@ -6,8 +7,8 @@ export default function Document() {
     <Html lang="en">
       <Head>
       <link rel="preconnect" href="https://fonts.googleapis.com"/>
-<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
-<link href="https://fonts.googleapis.com/css2?family=DM+Sans&family=Inter:wght@100;300;400;500;600;700;800;900&display=swap" rel="stylesheet"/>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+      <link href="https://fonts.googleapis.com/css2?family=DM+Sans&family=Inter:wght@100;300;400;500;600;700;800;900&display=swap" rel="stylesheet"/>
 </Head>
       <body className='font-inter'>
 
